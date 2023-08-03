54a1903445f7637e473ca3eedbd13c9996970059
Gustavo Barboza <gustavobarboza2003@gmail.com>
2023-08-02 21:03:45 -0300

    Js Modal

--- main.js
+++ main.js
@@ -1,21 +1,9 @@
+import { Modal } from './modal.js'
+
 const form = document.querySelector('form')
 const inputWeigth = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
 
-
-const Modal = {
-  wrapper: document.querySelector('.modal-wrapper'),
-  message: document.querySelector('.modal .title span'),
-  buttonClose:document.querySelector('.modal button.close'),
-
-  
-  open() {
-    Modal.wrapper.classList.add('open')
-  },
-  close() {
-    Modal.wrapper.classList.remove('open')
-  }
-}
 
 form.onsubmit = event => {
   event.preventDefault()
@@ -28,10 +16,6 @@ form.onsubmit = event => {
   
   Modal.message.innerText = massage 
   Modal.open()
-}
-
-Modal.buttonClose.onclick = () => {
-  Modal.close()
 }
 
 function IMC(weight, height) {
