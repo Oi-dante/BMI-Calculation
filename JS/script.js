bf4b5b8b39202890a4a2713d7ba9d40000f52f05
Gustavo Barboza <gustavobarboza2003@gmail.com>
2023-08-03 23:31:22 -0300

    clean code

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,6 @@
 import { Modal } from './modal.js'
 import { AlertError } from './alert-error.js'
+import { calculateIMC, notANumber } from './utils.js'
 
 const form = document.querySelector('form')
 const inputWeigth = document.querySelector('#weight')
@@ -12,26 +13,24 @@ form.onsubmit = event => {
   const weight = inputWeigth.value
   const height = inputHeight.value
 
-  const showAlertErro = notANumber(weight) || notANumber(height)
+  const weightOrHeightIsNotANumber = notANumber(weight) || notANumber(height)
 
-  if (showAlertErro){
+  if (weightOrHeightIsNotANumber ){
     AlertError.open()
     return;
   }
 
   AlertError.close()
 
-  const result = IMC(weight, height)
+  const result = calculateIMC(weight, height)
+  displayResultMessage(result)
+ 
+}
+
+function displayResultMessage(result){
   const massage = `Seu IMC é de ${result}`
   
   Modal.message.innerText = massage 
   Modal.open()
 }
 
-function notANumber(value){
-  return isNaN(value) || value == ""
-}
-
-function IMC(weight, height) {
-  return (weight / ((height / 100) ** 2)).toFixed(2)
-}
